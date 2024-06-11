151ce6cd4ea173f31388893d7a280a3d6de18c67
Oganyan Semen <zeaist@mail.ru>
2024-06-11 05:24:59 +0500

    new

--- src/App.java
+++ src/App.java
@@ -1,8 +1,19 @@
 public class App {
     public static void main(String[] args){
 // Первое задание
-        Cat cat = new Cat("Барсик");
-        cat.run(100); // Проверка кота на бег
-        cat.swim(); // Проверка кота на плавание
+        Cat[] arrCat = new Cat[2];
+        arrCat[0] = new Cat("Барсик");
+        arrCat[1] = new Cat("Снежок");
+        arrCat[2] = new Cat("Матроскин");
+
+        Dog[] arrDog = new Dog[1];
+        arrDog[0] = new Dog("Печенег");
+
+        arrCat[0].run(100); // Проверка кота на бег
+        arrCat[1].swim(); // Проверка кота на плавание
+
+        arrDog[0].run(1000); // Проверка пса на бег
+        arrDog[0].swim(6); // Проверка пса на плавание
+
     }
 }
